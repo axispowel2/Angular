8b6ed6cffe12d4814357d8165c81846cefae3529
Sarawut <sarawut_yeen@hotmail.com>
2017-10-26 14:01:08 +0700

    Add file Homework6(Angular)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,18 +8,20 @@ import { FormsModule } from '@angular/forms';
 import { GetphotoService } from './services/getphoto.service';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
+import { MostPopularMovie2018Component } from './components/most-popular-movie2018/most-popular-movie2018.component';
 
 const appRoutes: Routes = [
   { path: "", component: UserComponent },
-  { path: "about", component: AboutusComponent }
-
+  { path: "about", component: AboutusComponent },
+  { path: "movie2018", component: MostPopularMovie2018Component }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    AboutusComponent
+    AboutusComponent,
+    MostPopularMovie2018Component
   ],
   imports: [
     BrowserModule,
